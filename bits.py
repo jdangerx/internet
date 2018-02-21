4f3d24e29c7d6bbd37c046c06d88134d94dd7894
John Xia <john.danger.xia@gmail.com>
2018-02-21 12:32:45 -0500

    A bunch of binary stuff to get the checksum working.

---
+++ bits.py
@@ -0,0 +1,99 @@
+import itertools
+
+def bytes_to_ints(bs):
+    """
+    Convert a list of bytes to a list of integers.
+
+    >>> bytes_to_ints([1, 0, 2, 1])
+    [256, 513]
+    >>> bytes_to_ints([1, 0, 1])
+    Traceback (most recent call last):
+        ...
+    ValueError: Odd number of bytes.
+    >>> bytes_to_ints([])
+    []
+    """
+    if len(bs) % 2 != 0:
+        raise ValueError("Odd number of bytes.")
+    pairs = zip(bs[::2], bs[1::2])
+    return [(a << 8) + b for a, b in pairs]
+
+
+def sixteen_to_eight(xs):
+    """
+    Convert a list of integers to a list of bytes.
+
+    >>> sixteen_to_eight([256, 513])
+    [1, 0, 2, 1]
+    >>> sixteen_to_eight([])
+    []
+    """
+    arr = itertools.chain.from_iterable((x >> 8, x & 255) for x in xs)
+    return list(arr)
+
+
+def ones_complement_addition(x, y, bitsize=16):
+    """
+    Add two numbers of any bitsize and carry the carry around.
+
+    11 + 10 = 101 => 10:
+    >>> ones_complement_addition(3, 2, 2)
+    2
+
+    11 + 11 = 110 => 11:
+    >>> ones_complement_addition(3, 3, 2)
+    3
+
+    00 + 10 = 10 => 10:
+    >>> ones_complement_addition(0, 2, 2)
+    2
+    """
+    cap = 2 ** bitsize - 1
+    total = x + y
+    if total > cap:
+        total -= cap
+    return total
+
+
+def ones_complement_sum(xs, bitsize=16):
+    """
+    Sum across a bunch of integers with one's complement.
+
+    [11, 10, 10] -> [10, 10] -> [01]:
+    >>> ones_complement_sum([3, 2, 2], 2)
+    1
+
+    [10, 10, 10] -> [01, 10] -> [11]:
+    >>> ones_complement_sum([2, 2, 2], 2)
+    3
+
+    []:
+    >>> ones_complement_sum([], 2)
+    0
+    """
+    sum = 0
+    for x in xs:
+        sum = ones_complement_addition(sum, x, bitsize)
+    return sum
+
+
+def ones_complement(x, bitsize=16):
+    """
+    Swap all 1s and 0s.
+
+    01 -> 10
+    >>> ones_complement(1, bitsize=2)
+    2
+
+    >>> ones_complement(7, bitsize=3)
+    0
+
+    >>> ones_complement((ones_complement(15232)))
+    15232
+    """
+    return ((1 << bitsize) - 1) ^ x
+
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
--- ping.py
+++ ping.py
@@ -1,26 +1,46 @@
 #! /usr/bin/env python3
-"Rudimentary ping."
+"Rudimentary ping using sockets."
 
-from scapy.all import IP, ICMP, sr1
-import time
+import socket
+
+from bits import bytes_to_ints, ones_complement_sum, ones_complement
+
+
+def checksum(bs):
+    ints = bytes_to_ints(bs)
+    ones_comp_sum = ones_complement_sum(ints)
+    return ones_complement(ones_comp_sum)
 
 
 def ping(dest, seq):
-    payload = bytes(list(range(48)))
-    echo_request = (IP(dst=dest) /
-                    ICMP(id=1, seq=seq) /
-                    payload)
-    start = time.time()
-    response = sr1(echo_request)
-    response_ms = (time.time() - start) * 1000
-    icmp = response.payload
-    response_len = len(response)
-    print("{response_len} bytes from {response.src}: "
-          "icmp_seq= {icmp.seq} "
-          "ttl= {response.ttl} "
-          "time={response_ms:.2f} ms".format(**locals()))
+    icmp = socket.getprotobyname('icmp')
+    sock = socket.socket(
+        family=socket.AF_INET,
+        type=socket.SOCK_RAW,
+        proto=icmp
+    )
+    icmp_type = bytes([8])
+    icmp_code = bytes([0])
+    checksum = bytes([0, 0])
+    identifier = sixteen_to_eight([1])
+    sequence_number = sixteen_to_eight([seq])
+    data = bytes(list(range(32)))
+    payload = b"".join(
+        icmp_type,
+        icmp_code,
+        checksum,
+        identifier,
+        sequence_number,
+        data
+    )
+
+    sock.sendto(payload, (dest, 1))
 
 
 if __name__ == "__main__":
     import sys
-    ping(sys.argv[1], 1)
+    if sys.argv[1] == "test":
+        import doctest
+        doctest.testmod()
+    else:
+        ping(sys.argv[1], 1)
